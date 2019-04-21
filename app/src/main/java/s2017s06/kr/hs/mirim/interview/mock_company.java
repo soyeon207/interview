ac217f0d070b35355485720fbd178e345aaafb64
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-04-21 20:47:13 +0900

    add intent in mock_company

--- app/src/main/java/s2017s06/kr/hs/mirim/interview/mock_company.java
+++ app/src/main/java/s2017s06/kr/hs/mirim/interview/mock_company.java
@@ -1,13 +1,19 @@
 package s2017s06.kr.hs.mirim.interview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class mock_company extends AppCompatActivity {
+    String id, pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mock_company);
+
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+        pwd = intent.getStringExtra("pwd");
     }
 }
