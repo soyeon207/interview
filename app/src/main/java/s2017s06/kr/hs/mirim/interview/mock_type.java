68897b19a1b581ab24317b2f450bcb6270fe08fd
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-05-24 01:27:41 +0900

    surfaceView camera

--- app/src/main/java/s2017s06/kr/hs/mirim/interview/mock_type.java
+++ app/src/main/java/s2017s06/kr/hs/mirim/interview/mock_type.java
@@ -1,15 +1,23 @@
 package s2017s06.kr.hs.mirim.interview;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,7 +25,11 @@ import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -26,7 +38,7 @@ import java.util.Random;
 public class mock_type extends AppCompatActivity {
     String id, pwd, type;
 
-    private Button camera;
+    //private Button camera;
     private Button next;
     private Button end;
 
@@ -37,8 +49,35 @@ public class mock_type extends AppCompatActivity {
     int i = 0;
     int j = 0;
 
-    private TextToSpeech tts;
-    private Button btSpeak;
+    TextView question;
+
+    /*private TextToSpeech tts;
+    private Button btSpeak;*/
+
+    //카메라
+
+    private final int PERMISSIONS_ALL = 5;
+
+    SurfaceView surfaceView;
+    SurfaceHolder holder;
+    MediaRecorder recorder;
+
+    Camera camera;
+
+    SimpleDateFormat format = new SimpleDateFormat("yyMMdd_HHmmss");
+
+    long now;
+    Date time;
+    String time_name;
+    String path;
+
+    private String getTime() {
+        now = System.currentTimeMillis();
+        time = new Date(now);
+        return format.format(time);
+    }
+
+    //카메라
 
     private String[] app = {"작년 방과후 시간에 무엇을 했나?", "어노테이션(@)와 주석의 차이는?", "디자인 패턴을 알고 있는가?", "안드로이드는 언제 배웠나요?", "ios를 배우고 싶은 마음은?",
             "알고 있는 단축키는?", "가장 잘하는 언어와 그 이유?", "안드로이드는 언제 배웠나요?", "ios를 배우고 싶은 마음은 있나요?"};
@@ -49,6 +88,40 @@ public class mock_type extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mock_type);
+
+        //권한 및 카메라
+
+        String str = Environment.getExternalStorageState();
+        if (str.equals(Environment.MEDIA_MOUNTED)) {
+
+            String dirPath = "/sdcard/DCIM/모면";
+            File file = new File(dirPath);
+            if (!file.exists())  // 원하는 경로에 폴더가 있는지 확인
+                file.mkdirs();
+        } else
+            Toast.makeText(this, "SD Card 인식 실패", Toast.LENGTH_SHORT).show();
+
+        int permssionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int permssionRecord = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        int permssionRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permssionWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if ((permssionCamera != PackageManager.PERMISSION_GRANTED) && (permssionRecord != PackageManager.PERMISSION_GRANTED) && (permssionRead != PackageManager.PERMISSION_GRANTED) && (permssionWrite != PackageManager.PERMISSION_GRANTED)) {
+            Toast.makeText(this, "권한 승인이 필요합니다.", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "면접을 녹화하기 위해 카메라, 오디오 녹음 등의 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_ALL);
+                Toast.makeText(this, "면접을 녹화하기 위해 카메라 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+
+        holder = surfaceView.getHolder();
+
+        //권한 및 카메라
 
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
@@ -69,16 +142,15 @@ public class mock_type extends AppCompatActivity {
 
         //tts = new TextToSpeech(this, this); //첫번째는 context, 두번째는 리스너
 
-        btSpeak = (Button) findViewById(R.id.speak);
+        //btSpeak = (Button) findViewById(R.id.speak);
 
-        final TextView question=findViewById(R.id.question);
+        question=findViewById(R.id.question);
 
         switch (type) {
             case "app":
                 random  = rnd.nextInt(app.length);
                 qChar = app[random].toCharArray();
                 question.setText(qChar, 0, qChar.length);
-                j = i;
                 i++;
                 break;
 
@@ -110,14 +182,14 @@ public class mock_type extends AppCompatActivity {
                 break;
         }
 
-        btSpeak.setOnClickListener(new View.OnClickListener() {
+        /*btSpeak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 speakOutNow();
             }
-        });
+        });*/
 
-        camera=(Button)findViewById(R.id.camera);
+        /*camera=(Button)findViewById(R.id.camera);
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -126,7 +198,7 @@ public class mock_type extends AppCompatActivity {
                     startActivityForResult(cameraApp, 10000);
                 }
             }
-        });
+        });*/
 
         next=findViewById(R.id.btn_next);
         next.setOnClickListener(new View.OnClickListener() {
@@ -192,7 +264,76 @@ public class mock_type extends AppCompatActivity {
         });
     }
 
-    private boolean isExistsCameraApplication(){
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_ALL: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "승인이 허가되어 있습니다.", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(this, "아직 승인받지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+        }
+    }
+
+    public void BtnStart(View v) {
+        time_name = getTime();
+
+        try {
+            if (recorder != null) {
+                recorder.stop();
+                recorder.release();
+                recorder = null;
+            }
+
+            recorder = new MediaRecorder();
+
+            camera = Camera.open(1);
+            camera.setDisplayOrientation(90);
+            camera.unlock();
+
+            recorder.setCamera(camera);
+
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+
+            path = "/sdcard/DCIM/모면/" + time_name + ".mp4";
+
+            recorder.setOutputFile(path);
+            recorder.setPreviewDisplay(holder.getSurface());
+            recorder.prepare();
+            recorder.start();
+
+            Toast.makeText(getApplicationContext(), "녹화를 시작합니다.", Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void BtnStop(View v) {
+        if (recorder != null) {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+
+            camera.stopPreview();
+            camera.release();
+        }
+
+        Toast.makeText(getApplicationContext(), "녹화를 중지합니다.", Toast.LENGTH_LONG).show();
+    }
+
+    /*private boolean isExistsCameraApplication(){
         PackageManager packageManager = getPackageManager();
 
         Intent cameraApp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -200,11 +341,11 @@ public class mock_type extends AppCompatActivity {
         List<ResolveInfo> cameraApps = packageManager.queryIntentActivities(cameraApp, PackageManager.MATCH_DEFAULT_ONLY);
 
         return cameraApps.size() > 0;
-    }
+    }*/
 
     //앱종료시 tts를 같이 종료해 준다.
-    @Override
-    protected void onDestroy() {
+    //@Override
+    /*protected void onDestroy() {
         if (tts != null) {
             tts.stop();
             tts.shutdown();
@@ -235,6 +376,6 @@ public class mock_type extends AppCompatActivity {
         //tts.setPitch((float) 0.1); //음량
         //tts.setSpeechRate((float) 0.5); //재생속도
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-    }
+    }*/
 
 }
